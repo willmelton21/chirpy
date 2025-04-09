73d9a4a7f02e93ecc59e1db2072482b058a3fb3a
Will <meltonjw3@gmail.com>
2025-04-08 20:30:01 -0500

    added middleware func as well as endpoints for count and reset

--- main.go
+++ main.go
@@ -3,13 +3,41 @@ package main
 import (
 	"net/http"
 	"fmt"
+   "sync/atomic"	
 )
+
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w,r)
+	})
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprintf(w, "Hits: %d", cfg.fileserverHits.Load())
+}
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+
+	cfg.fileserverHits.Store(0)
+}
 
 func main() {
 
 	mux := http.NewServeMux()
+	var apiCfg apiConfig
 
-	mux.Handle("/app/",http.StripPrefix("/app/",http.FileServer(http.Dir('.'))))
+	handler := http.StripPrefix("/app/",http.FileServer(http.Dir('.')))
+
+
+	mux.Handle("/app/",apiCfg.middlewareMetricsInc(handler))
 	servStruct := http.Server{
 		Handler: mux,
 		Addr: ":8080",
@@ -21,6 +49,9 @@ func main() {
 		w.Write([]byte("OK"))
 	})
 
+	mux.HandleFunc("/metrics",apiCfg.metricsHandler)
+	mux.HandleFunc("/reset",apiCfg.resetHandler)
+	
 	err := servStruct.ListenAndServe()
 
 	fmt.Print("err is: ",err)
