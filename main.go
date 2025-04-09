f789df5dc072cfffae557782910ec21112e807a4
Will <meltonjw3@gmail.com>
2025-04-08 19:38:50 -0500

    added image asset, added /healthz check and moved hello world page to /app

--- main.go
+++ main.go
@@ -9,10 +9,18 @@ func main() {
 
 	mux := http.NewServeMux()
 
+	mux.Handle("/app/",http.StripPrefix("/app/",http.FileServer(http.Dir('.'))))
 	servStruct := http.Server{
 		Handler: mux,
 		Addr: ":8080",
 	}
+	mux.HandleFunc("/healthz",func(w http.ResponseWriter, req *http.Request) {
+		
+		w.Header().Set("Content_Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
 	err := servStruct.ListenAndServe()
 
 	fmt.Print("err is: ",err)
